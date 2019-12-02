924308bdc09627e0bcb9aac24081de10aeecb294
kelinlang <kelinlang@163.com>
2019-12-02 22:28:19 +0800

    调试

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <cstdio>
+extern "C" {
 
+#include "libavformat/avformat.h"
+#include "libavdevice/avdevice.h"
+#include "libswresample/swresample.h"
+#include "libavutil/opt.h"
+#include "libavutil/channel_layout.h"
+#include "libavutil/parseutils.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/fifo.h"
+#include "libavutil/hwcontext.h"
+//#include "libavutil/internal.h"
+#include "libavutil/intreadwrite.h"
+#include "libavutil/dict.h"
+#include "libavutil/display.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/avstring.h"
+//#include "libavutil/libm.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/timestamp.h"
+#include "libavutil/bprint.h"
+#include "libavutil/time.h"
+//#include "libavutil/thread.h"
+#include "libavutil/threadmessage.h"
+//#include "libavcodec/mathops.h"
+//#include "libavformat/os_support.h"
+
+# include "libavfilter/avfilter.h"
+# include "libavfilter/buffersrc.h"
+# include "libavfilter/buffersink.h"
+}
 int main()
 {
     printf("hello from FFmpegDemo!\n");
+
+	avformat_network_init();
+	printf("hello from FFmpegDemo  finish!\n");
+
     return 0;
 }
